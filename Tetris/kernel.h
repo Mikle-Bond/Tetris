ffac022b2b4b5f88209a1586a64d77301b891186
Mikle-Bond <Mikle_Bond@mail.ru>
2015-05-11 23:11:37 +0300

    [KERNEL] Score system adding

--- Tetris/kernel.h
+++ Tetris/kernel.h
@@ -5,6 +5,7 @@
 #define KERNEL
 
 #include "kernel_types.h"
+#include "score_system.h"
 
 /*
 struct flags {
@@ -15,9 +16,20 @@ struct flags {
 } flag;
 */
 
+//---------------------------------------------------------
+// Create new tetrimino on the top
+extern void tet_create (tet_num type, direction angle);
+/* Usage:
+ *      Placeing new particle at the service
+ *      zone and changes cintrol to the new one
+ * Example:
+ *      create (tet_I, left);
+ *      create (rand() % 4, rand() % 4); // not recommended
+ */
 
+//---------------------------------------------------------
 // Move falling particle
-extern err_move move (direction d);
+extern err_move tet_move (direction d);
 /* Usage:
  *      Will try to move particle according
  *      to the direction and repace if success.
@@ -31,21 +43,22 @@ extern err_move move (direction d);
  *      move (up);    // lol
  */
 
-// Create new tetrimino on the top
-extern void create (tet_num type, direction angle);
+//---------------------------------------------------------
+// Rotate the particle
+extern err_move tet_rotate (int side);
 /* Usage:
- *      Placeing new particle at the service
- *      zone and changes cintrol to the new one
+ *      Will try to rotate current particle, and if it
+ *      is possible - will rotate it. Else - non-zero
+ *      value will be returned.
+ *      Arguments is +1 to rotate right and -1 to left
+ *
  * Example:
- *      create (tet_I, left);
- *      create (rand() % 4, rand() % 4); // not recommended
+ *      rotate (1);
  */
 
-// Rotate the particle
-extern err_move rotate (int side);
-
+//---------------------------------------------------------
 // Global matrix
-extern atom_pixel glob_map[35][12];
+extern matrix glob_map;
 /* global matrix 24x10 pixels
  * plus 4 rows on the top
  * plus two rows and colums on the bottom-right
@@ -56,14 +69,62 @@ extern atom_pixel glob_map[35][12];
  * visible:     [height_T + 4][width_L] .. [height_B][width_R]
  */
 
-const int width_L =     1;
-const int width_R =     10;
-const int height_T =    1;
-const int height_B =    32;
+// Constants to divide (glob_map)
+extern const int width_L;
+extern const int width_R;
+extern const int height_T;
+extern const int height_B;
+
+//---------------------------------------------------------
+// Place and stabialize (curr)
+extern void tet_stop ();
+/* Usage:
+ *      Needs to be called at the end of falling.
+ *      Normally, it should be called when move(down)
+ *      returns non-zero value.
+ *      After calling, controling current tetrimino would
+ *      be not possible, until create(...) is called.
+ *      Also, clear_all_lines() is avaluable only after
+ *      this function.
+ */
+
+//---------------------------------------------------------
+// Deleting all full rows
+extern void clear_all_rows ();
+/* Usage:
+ *      Deleting all fulfiled rows from the global map.
+ *      Should be called only after tet_stop().
+ */
+
+//---------------------------------------------------------
+// Check the state of line 
+extern char get_row_state (char number);
+/* Usage:
+ *      Returns state of line's fulfillness by its nmber.
+ *      Here (number) is allowed oly from (height_T)
+ *      to (height_B).
+ * Example:
+ *      get_row_state (height_T + row);
+ */
+
+//---------------------------------------------------------
+// Sets state of the row to (value)
+extern void set_row_state (char number, char value);
+/* Usage:
+ *      WARNING: Not recommended to use it.
+ *
+ *      Set the row state to 0, if it is not filled,
+ *      and 1, in the other case.
+ */
+
+
+//---------------------------------------------------------
+// Save section
+//---------------------------------------------------------
+extern particle curr;
+extern row_filler row_index;
 
 /// DBG
-extern particle curr;
-
 extern void dbg_dump ();
 
 #endif // KERNEL
--- Tetris/kernel_types.h
+++ Tetris/kernel_types.h
@@ -1,15 +1,27 @@
 #ifndef KERNEL_TYPES
 #define KERNEL_TYPES
 
+#include <inttypes.h>
 
 // type of single element on map
 typedef unsigned char atom_pixel;
+
+// type of global matrix
+typedef atom_pixel matrix[35][12];
 
 // directions
 typedef enum direction_t { down, left, up, right } direction;
 
 // list of the particles
-typedef enum tet_num_t { tet_B, tet_L, tet_J, tet_S, tet_Z, tet_I, tet_T } tet_num;
+typedef enum tet_num_t {
+    tet_B = 1,
+    tet_L,
+    tet_J,
+    tet_S,
+    tet_Z,
+    tet_I,
+    tet_T
+} tet_num;
 
 // current falling particle
 typedef struct {
@@ -37,6 +49,10 @@ typedef enum err_move_t {
     ovflw_T,            // out of matrix borders
     flood_anoter,       // meeting others
 } err_move;
+
+// struct of states of each line
+typedef uint64_t row_filler;
+
 
 #endif // KERNEL_TYPES
 
